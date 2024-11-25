a9dc9960e4fc35aa1d1eb39e756631cfe4dddaec
melih kaan araç <melihkaanarac@gmail.com>
2024-11-25 04:17:34 +0300

    CREATE: delete update set

---
+++ MvcCv/Repositories/HobilerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MvcCv.Models.Entity;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    public class HobilerRepository : GenericRepositroy<TblHobilerim>
+    {
+    }
+}
