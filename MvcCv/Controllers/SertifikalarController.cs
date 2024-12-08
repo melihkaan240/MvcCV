cf94128fabde0ddadf0d8a684d1c9109f70944cf
melih kaan araç <melihkaanarac@gmail.com>
2024-12-09 02:40:56 +0300

    FIX: sertiifkalar

---
+++ MvcCv/Controllers/SertifikalarController.cs
@@ -0,0 +1,21 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class SertifikalarController : Controller
+    {
+        // GET: Sertifikalar
+        GenericRepositroy<TblSertifikalarım> repo = new GenericRepositroy<TblSertifikalarım>();
+        public ActionResult Index()
+        {
+            var sertifikalar = repo.List();
+            return View(sertifikalar);
+        }
+    }
+}
